2de935fee01ff695f50dab5a101300e01ce295d9
Tomasz Molis <molitom@chrobinson.com>
2024-09-27 11:44:09 +0200

    profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,11 @@
 import { MyData } from '../../components/profile/my-data';
+import { Member } from '../../models/members';
 
 export default async function ProfilePage() {
+    const member:Member = {}as Member;
     return (
         <div className="w-full">
-            <MyData />
+            <MyData  data={member}/>
         </div>);
 
 }
--- components/profile/my-data.tsx
+++ components/profile/my-data.tsx
@@ -7,8 +7,13 @@ import { DatePicker } from "@nextui-org/date-picker";
 import { Input } from "@nextui-org/input";
 import { useState } from "react";
 import { EditableInput } from "../common/editable-input";
+import { Member } from "../../models/members";
 
-export function MyData() {
+interface MyDataProps {
+    data:Member
+}
+
+export function MyData(props:MyDataProps) {
     const [editEmail, setEditEmail] = useState(false);
     const [editPhone, setEditPhone] = useState(false);
     const [editBirth, setEditBirth] = useState(false);
@@ -27,8 +32,8 @@ export function MyData() {
                     </div>
                 </div>
                 <div className="grid grid-cols-12 gap-2">
-                    <EditableInput label="Imię" name="given_name" disabled fieldType="text" onUpdate={(value) => console.log(value)} />
-                    <EditableInput label="Nazwisko" name="family_name" disabled fieldType="text" onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Imię" name="given_name" disabled value={props.data.firstName} fieldType="text" onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Nazwisko" name="family_name" value={props.data.firstName} disabled fieldType="text" onUpdate={(value) => console.log(value)} />
                     <DatePicker
                         label="Data urodzenia"
                         name="date"
@@ -44,18 +49,18 @@ export function MyData() {
                     <h2 className="col-span-12">
                         Dane kontaktowe
                     </h2>
-                    <EditableInput label="Telefon" name="phone" fieldType="tel" onUpdate={(value) => console.log(value)} />
-                    <EditableInput label="Email" name="email" fieldType="email" onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Telefon" name="phone" fieldType="tel" value={props.data.phone}  onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Email" name="email" fieldType="email" value={props.data.email} onUpdate={(value) => console.log(value)} />
 
                     <h3>Address</h3>
 
-                    <EditableInput label="Kraj" size="md" name="country" fieldType="text" onUpdate={(value) => console.log(value)} />
-                    <EditableInput label="Region" size="md" name="region" fieldType="text" onUpdate={(value) => console.log(value)} />
-                    <EditableInput label="Miasto" size="md" name="city" fieldType="text" onUpdate={(value) => console.log(value)} />
-                    <EditableInput label="Kod pocztowy" size="md" name="postal_code" fieldType="text" onUpdate={(value) => console.log(value)} />
-                    <EditableInput label="Ulica" size="md" name="city" fieldType="text" onUpdate={(value) => console.log(value)} />
-                    <EditableInput label="Nr domu" size="sm" name="postal_code" fieldType="text" onUpdate={(value) => console.log(value)} />
-                    <EditableInput label="Nr mieszkania" size="sm" name="postal_code" fieldType="text" onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Kraj" size="md" name="country" fieldType="text" value={props.data.address.country} onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Region" size="md" name="region" fieldType="text" value={props.data.address.region} onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Miasto" size="md" name="city" fieldType="text" value={props.data.address.city} onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Kod pocztowy" size="md" name="postal_code" value={props.data.address.postal_code} fieldType="text" onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Ulica" size="md" name="city" fieldType="text"value={props.data.address.street} onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Nr domu" size="sm" name="postal_code" fieldType="text" value={props.data.address.house} onUpdate={(value) => console.log(value)} />
+                    <EditableInput label="Nr mieszkania" size="sm" name="postal_code" fieldType="text" value={props.data.address.apartment} onUpdate={(value) => console.log(value)} />
                 </div>
 
             </CardBody>
